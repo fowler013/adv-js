463ba057c7540704bc7571a92fa91dda33045c4f
fowler013 <fowler0613@gmail.com>
2018-05-23 14:16:34 -0500

    initial commit

---
+++ adv.js
@@ -0,0 +1,61 @@
+thisFunc()
+function thisFunc(){
+    console.log('Hello Tevin')
+}
+newFunc();
+let myfun = 'new';
+function newFunc(){
+ 
+    alert(myfun);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let fruits = ['grapes', 'apples', 'melons'];
+// let favFruit;
+// let leastFav;
+// function printFruits(){
+//     let favFruit = fruits[1];
+//     let leastFav = fruits[2]
+//     console.log(fruits[0]);
+
+//     function printfavFruit(){
+//     console.log(favFruit);
+//     }
+// }
+
+
+
+// printFruits();
+// printfavFruit();
+//  console.log(leastFav);
+
+
+
+
+
+
+
+
+// name = 'Tevin'
+// var name;
+// console.log(name);
+
+// setName()
+// function setName(){
+//     var name = 'Fowler';
+//     console.log(name);
+// }
